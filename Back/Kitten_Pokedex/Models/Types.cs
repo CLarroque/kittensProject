c02139b45271a9c78040116754abf80c95b36384
Adrien3111 <44834870+Adrien3111@users.noreply.github.com>
2022-06-02 13:25:35 +0200

    Add files via upload

---
+++ Back/Kitten_Pokedex/Models/Types.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kitten_Pokedex.Models
+{
+    public class Types
+    {
+        public string english { get; set; }
+        public string chinese { get; set; }
+        public string japanese { get; set; }
+        public string french { get; set; }
+
+    }
+}

cbb05024310091b0e06a50cb1b3c8525dbc2d2be
Adrien BOURREL <abourrel@mcgr.local>
2022-06-02 13:28:30 +0200

    e

---
+++ Kitten_Pokedex/Controllers/PokemonController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kitten_Pokedex.Controllers
+{
+    [Route("pokemons/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+
+        //GET: pokemons
+
+        //GET: pokemons/:id
+
+        //GET: pokemons/:type
+
+        //GET: pokemons/:name
+
+        //POST: pokemons 
+
+
+
+        //POST: pokemons
+
+        //PUT: pokemons
+        
+    }
+}
